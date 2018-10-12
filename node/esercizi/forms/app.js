0b57b36aa909476e4d974d149d676ff6d67a8535
Golim <matteogolinelli97@gmail.com>
2018-10-12 14:12:40 +0200

    Completed node exercises

--- node/esercizi/forms/app.js
+++ node/esercizi/forms/app.js
@@ -1,11 +1,11 @@
 /*
-* app.js
-* Main entry point of the forms project
-* This script shows how to create a server
-* that can handle request from web forms
-*/
+ * app.js
+ * Main entry point of the forms project
+ * This script shows how to create a server
+ * that can handle request from web forms
+ */
 var express = require('express');
-var app  = express();
+var app = express();
 
 // Loading utils to inspect the content of js objects
 var util = require('util');
@@ -14,29 +14,69 @@ var port = 3000;
 
 app.use('/', express.static('public'));
 
-var people = [{ name : "Mario Ferrari", email : "fake@news.it"}, 
-              { name : "Carlo Smith",   email : "youreach@menot.it"},
-              { name : "Fabio Ferrari", email : "email@email.com"}];
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+
+
+
+
+var people = [{
+    name: "Mario Ferrari",
+    email: "fake@news.it"
+  },
+  {
+    name: "Carlo Smith",
+    email: "youreach@menot.it"
+  },
+  {
+    name: "Fabio Ferrari",
+    email: "email@email.com"
+  }
+];
 
 // Handling GET requests
-app.get('/search', function(req, res){ 
-  
-  console.log(util.inspect(req.headers, {showHidden: false, depth: null}))
-  console.log(util.inspect(req.url, {showHidden: false, depth: null}))
-  console.log(util.inspect(req.query, {showHidden: false, depth: null}))
-  
-  res.status(200).send('These are the items found!');  
+app.get('/search', function (req, res) {
+  var items = "";
+
+  items = "<table style=\"border: 1px solid black;\"><tr style=\"border: 1px solid black;\"><th>Name</th><th>Email</th></tr>"
+
+  people.forEach(p => {
+
+    items += "<tr><th>" + p.name + "</th><th>" + p.email + "</th></tr>";
+  });
+
+  items += "</table>";
+
+  res.status(200).send('These are the items found!<br><br>' + items);
 });
 
-app.post('/subscribe', function(req, res){ 
+app.post('/subscribe', function (req, res) {
+  var name = req.body.name;
+  var email = req.body.email;
 
-  console.log(util.inspect(req.headers, {showHidden: false, depth: null}))
-  console.log(util.inspect(req.params, {showHidden: false, depth: null}))  
-  
-  res.status(201).send('You are now subscribed!');
-  
+  var present = false;
+  people.forEach(p => {
+    if (!present && p.email === email)
+      present = true;
+  });
+
+  if (!present) {
+    people.push({
+      name: name,
+      email: email
+    });
+
+    res.status(201).send('You are now subscribed!');
+  } else {
+    res.status(201).send('You are yet subscribed!');
+  }
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log('Server running on port ', port);
 });
